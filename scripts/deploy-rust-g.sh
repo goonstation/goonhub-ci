94f92c6ee932509b4a3249678450e34f75b4b85e
Wire <wirewraith@gmail.com>
2023-09-19 09:43:09 -0400

    Update rust-g deployment

--- scripts/deploy-rust-g.sh
+++ scripts/deploy-rust-g.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
-cd /rust-g/goonstation-rust-g/target/i686-unknown-linux-gnu/release
+cd /rust-g/target/i686-unknown-linux-gnu/release
 
 cp librust_g.so /remote_ss13/servers/main1/game/update
 cp librust_g.so /remote_ss13/servers/main2/game/update
 cp librust_g.so /remote_ss13/servers/main3/game/update
 cp librust_g.so /remote_ss13/servers/main4/game/update
+cp librust_g.so /remote_ss13/servers/main5/game/update
 cp librust_g.so /remote_ss13/servers/dev/game/update
--- scripts/update-rust-g.sh
+++ scripts/update-rust-g.sh
@@ -8,13 +8,15 @@ echo "Fetching latest version"
 git fetch
 git reset --hard origin/master
 
+#echo "Applying patches"
+#./apply_patches.sh
+
 echo "Compiling rust-g"
 rustup target add i686-unknown-linux-gnu
 rustup update
-cd goonstation-rust-g
+#cd goonstation-rust-g
 export RUSTFLAGS="-C target-cpu=native"
 export PKG_CONFIG_ALLOW_CROSS=1
-./apply_patches.sh
 cargo build --release --target i686-unknown-linux-gnu
 
 echo "Rust-g update complete"
